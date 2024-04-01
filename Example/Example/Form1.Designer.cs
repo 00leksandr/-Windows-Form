3a65b5d1637067c8dbf2ba538e75c299b4817611
00leksandr <138134482+00leksandr@users.noreply.github.com>
2024-04-01 14:56:15 +0300

    Add files via upload

---
+++ Example/Example/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menu_open = new System.Windows.Forms.Button();
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.borsch_text = new System.Windows.Forms.Label();
+            this.varenuku_text = new System.Windows.Forms.Label();
+            this.cumpot_picture = new System.Windows.Forms.PictureBox();
+            this.varenuku_picture = new System.Windows.Forms.PictureBox();
+            this.borsch_picture = new System.Windows.Forms.PictureBox();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.cumpot_text = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cumpot_picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.varenuku_picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borsch_picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menu_open
+            // 
+            this.menu_open.Location = new System.Drawing.Point(401, 210);
+            this.menu_open.Name = "menu_open";
+            this.menu_open.Size = new System.Drawing.Size(350, 156);
+            this.menu_open.TabIndex = 0;
+            this.menu_open.Text = "Open Menu";
+            this.menu_open.UseVisualStyleBackColor = true;
+            this.menu_open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
+            this.bindingNavigator1.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.bindingNavigator1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem});
+            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
+            this.bindingNavigator1.Size = new System.Drawing.Size(1030, 27);
+            this.bindingNavigator1.TabIndex = 1;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(55, 24);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 27);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(441, 544);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 544);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Вперед";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // borsch_text
+            // 
+            this.borsch_text.AutoSize = true;
+            this.borsch_text.Font = new System.Drawing.Font("Matura MT Script Capitals", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.borsch_text.Location = new System.Drawing.Point(409, 321);
+            this.borsch_text.Name = "borsch_text";
+            this.borsch_text.Size = new System.Drawing.Size(350, 45);
+            this.borsch_text.TabIndex = 7;
+            this.borsch_text.Text = "Борщ (999 кристалів)";
+            this.borsch_text.Visible = false;
+            // 
+            // varenuku_text
+            // 
+            this.varenuku_text.AutoSize = true;
+            this.varenuku_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.varenuku_text.Location = new System.Drawing.Point(369, 318);
+            this.varenuku_text.Name = "varenuku_text";
+            this.varenuku_text.Size = new System.Drawing.Size(420, 39);
+            this.varenuku_text.TabIndex = 10;
+            this.varenuku_text.Text = "Вареники (300 кристалів)";
+            this.varenuku_text.Visible = false;
+            this.varenuku_text.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cumpot_picture
+            // 
+            this.cumpot_picture.Image = global::Example.Properties.Resources.завантаження__1_;
+            this.cumpot_picture.Location = new System.Drawing.Point(401, 40);
+            this.cumpot_picture.Name = "cumpot_picture";
+            this.cumpot_picture.Size = new System.Drawing.Size(371, 275);
+            this.cumpot_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cumpot_picture.TabIndex = 11;
+            this.cumpot_picture.TabStop = false;
+            this.cumpot_picture.Visible = false;
+            // 
+            // varenuku_picture
+            // 
+            this.varenuku_picture.Image = global::Example.Properties.Resources.istockphoto_1061037198_612x612;
+            this.varenuku_picture.Location = new System.Drawing.Point(401, 40);
+            this.varenuku_picture.Name = "varenuku_picture";
+            this.varenuku_picture.Size = new System.Drawing.Size(358, 262);
+            this.varenuku_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.varenuku_picture.TabIndex = 9;
+            this.varenuku_picture.TabStop = false;
+            this.varenuku_picture.Visible = false;
+            // 
+            // borsch_picture
+            // 
+            this.borsch_picture.Image = global::Example.Properties.Resources.завантаження;
+            this.borsch_picture.Location = new System.Drawing.Point(409, 40);
+            this.borsch_picture.Name = "borsch_picture";
+            this.borsch_picture.Size = new System.Drawing.Size(350, 262);
+            this.borsch_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.borsch_picture.TabIndex = 8;
+            this.borsch_picture.TabStop = false;
+            this.borsch_picture.Visible = false;
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
+            // 
+            // cumpot_text
+            // 
+            this.cumpot_text.AutoSize = true;
+            this.cumpot_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cumpot_text.Location = new System.Drawing.Point(386, 318);
+            this.cumpot_text.Name = "cumpot_text";
+            this.cumpot_text.Size = new System.Drawing.Size(386, 39);
+            this.cumpot_text.TabIndex = 12;
+            this.cumpot_text.Text = "Кампот (150 кристалів)";
+            this.cumpot_text.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 606);
+            this.Controls.Add(this.cumpot_text);
+            this.Controls.Add(this.cumpot_picture);
+            this.Controls.Add(this.varenuku_text);
+            this.Controls.Add(this.varenuku_picture);
+            this.Controls.Add(this.borsch_picture);
+            this.Controls.Add(this.borsch_text);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bindingNavigator1);
+            this.Controls.Add(this.menu_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cumpot_picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.varenuku_picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borsch_picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button menu_open;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label borsch_text;
+        private System.Windows.Forms.PictureBox borsch_picture;
+        private System.Windows.Forms.PictureBox varenuku_picture;
+        private System.Windows.Forms.Label varenuku_text;
+        private System.Windows.Forms.PictureBox cumpot_picture;
+        private System.Windows.Forms.Label cumpot_text;
+    }
+}
+
---
+++ Example/Example/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int counter = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            borsch_picture.Visible = true;
+            menu_open.Visible = false;
+            button1.Visible = true;
+            button2.Visible = true;
+            counter++;
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter == 1)
+            {
+                borsch_picture.Visible = true;
+                borsch_text.Visible = true;
+                cumpot_picture.Visible = false;
+                cumpot_text.Visible = false;
+                varenuku_picture.Visible = false;
+                varenuku_text.Visible = false;
+                button2.Visible=false;
+                button1.Visible = true;
+
+            }
+            if (counter == 2)
+            {
+                borsch_picture.Visible = false;
+                borsch_text.Visible = false;
+                varenuku_picture.Visible = true;
+                varenuku_text.Visible = true;
+                cumpot_picture.Visible = false;
+                cumpot_text.Visible = false;
+                button2.Visible = true;
+                button1.Visible = true;
+            }
+            if (counter == 3)
+            {
+                varenuku_picture.Visible = false;
+                varenuku_text.Visible = false;
+                cumpot_picture.Visible = true;
+                cumpot_text.Visible = true;
+                borsch_picture.Visible = false;
+                borsch_text.Visible = false;
+                button2.Visible = true;
+                button1.Visible = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 1)
+            {
+                borsch_picture.Visible = true;
+                borsch_text.Visible = true;
+                cumpot_picture.Visible = false;
+                cumpot_text.Visible = false;
+                varenuku_picture.Visible = false;
+                varenuku_text.Visible = false;
+                button2.Visible = false;
+                button1.Visible = true;
+
+            }
+            if (counter == 2)
+            {
+                borsch_picture.Visible = false;
+                borsch_text.Visible = false;
+                varenuku_picture.Visible = true;
+                varenuku_text.Visible = true;
+                cumpot_picture.Visible = false;
+                cumpot_text.Visible = false;
+                button2.Visible = true;
+                button1.Visible = true;
+            }
+            if (counter == 3)
+            {
+                varenuku_picture.Visible = false;
+                varenuku_text.Visible = false;
+                cumpot_picture.Visible = true;
+                cumpot_text.Visible = true;
+                borsch_picture.Visible = false;
+                borsch_text.Visible = false;
+                button2.Visible = true;
+                button1.Visible = false;
+            }
+        }
+    }
+}
