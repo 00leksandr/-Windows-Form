e8e244f7b49db1d71b5eca21c34680ffe8890bc0
00leksandr <138134482+00leksandr@users.noreply.github.com>
2024-04-03 20:07:14 +0300

    Add files via upload

---
+++ ПР11 Наслідування та віртуальні методи/ПР11 Наслідування та віртуальні методи/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть свій вік: ");
+        int age = int.Parse(Console.ReadLine());
+
+        string[] listVaccins = { "Одноразова для дітей до 10 років", "Вакцина для підлітків", "Вакцина для повнолітніх", "Вакцина від деменції!!!" };
+
+        if (age < 10)
+        {
+            Console.WriteLine(listVaccins[0]);
+        }
+        else if (age >= 10 && age < 18)
+        {
+            Console.WriteLine(listVaccins[1]);
+        }
+        else if (age >= 18 && age < 25)
+        {
+            Console.WriteLine(listVaccins[2]);
+        }
+        else
+        {
+            Console.WriteLine(listVaccins[3]);
+        }
+    }
+}
