23fba5e56c96e66a84779928a0e1a5cb108b1457
00leksandr <138134482+00leksandr@users.noreply.github.com>
2024-04-09 12:25:52 +0300

    Add files via upload

---
+++ ПЗ10/ПЗ10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть емейл для перевірки:");
+        string email = Console.ReadLine();
+
+        if (IsValidEmail(email))
+        {
+            Console.WriteLine("Вірний формат :)");
+        }
+        else
+        {
+            Console.WriteLine("Не вірний формат ;(");
+        }
+    }
+
+    static bool IsValidEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        return Regex.IsMatch(email, pattern);
+    }
+}
